be52e4a9c9c2dc37014a79915a739687308e7af3
Amaan Gadatia <gadatia1@tcnj.edu>
2023-02-11 15:32:40 -0500

    Created student and course classes

---
+++ course.rb
@@ -0,0 +1,4 @@
+class Course
+    attr_accessor :course_num, :num_sections, :min_num_students, :max_num_students, :prereq_courses
+
+end
---
+++ student.rb
@@ -0,0 +1,3 @@
+class Student
+    attr_accessor :student_id, :num_units_done, :num_courses, :prereq_completed, :courses_desired
+end
